bf9203ea6a34c748b39e4510303267897aa86944
Dirlan Fernando <dirlanfr@gmail.com>
2023-12-14 21:15:45 -0300

    criando lógica

---
+++ src/components/ImputNumber.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="col">
+      <input
+        type="number"
+        placeholder="Digite um número"
+        class="p-2 rounded-3 form-control text-center"
+        value=""
+      />
+    </div>
+  </template>
